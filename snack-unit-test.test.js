2ef3ddb70becc74c9b6c920f6c6a84b6b6a8eb98
willymariino <wg.mariino@gmail.com>
2025-09-06 00:41:10 +0200

    snack 2

--- snack-unit-test.test.js
+++ snack-unit-test.test.js
@@ -1,3 +1,10 @@
+/*
+🏆 Snack 1
+Creare un test che verifichi la seguente descrizione:
+
+👉 "La funzione getInitials restituisce le iniziali di un nome completo."
+*/
+
 function getInitials(string) {
     return string
         .trim()
@@ -10,3 +17,18 @@ function getInitials(string) {
 test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
     expect(getInitials("Giorgio Rossi")).toBe("G.R")
 })
+
+/*
+🏆 Snack 2
+Creare un test che verifichi la seguente descrizione:
+
+👉 "La funzione createSlug restituisce una stringa in lowercase."
+*/
+
+function createSlug(string) {
+    return string.toLowerCase()
+}
+
+test("La funzione createSlug restituisce una stringa in lowercase.", () => {
+    expect(createSlug("STRINGA")).toBe("stringa")
+}) 
